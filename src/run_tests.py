f941d7d633bc9df592b9899f341654eea77e363c
jfgalamba <jfgmmg@mail>
2022-01-21 10:35:15 +0000

    Added more tests and refactored test code.

--- src/run_tests.py
+++ src/run_tests.py
@@ -1,18 +1,18 @@
+#!/usr/bin/env python3
 # pylint: disable=exec-used
 # pylint: disable=protected-access
 # pylint: disable=deprecated-method
 
-TEST_ALL = True
-
-from pydoc import plain
-import pycoder
 import io
+import pycoder
+
+TEST_ALL = True
 
 ################################################################################
 #
 #           UNIT TEST LIBRARY
 #
-#           Developed using class free and functional style OOP.
+#           Developed using class style OOP.
 #
 ################################################################################
 
@@ -27,10 +27,10 @@ class TestSuit:
         try:
             exec(f'assert {test_code}', None)
         except AssertionError:
-            print(f'{test_code:50} --> FAILED!')
+            print(f'{test_code:58} --> FAILED!')
             self.failed_count += 1
         else:
-            print(f'{test_code:50} OK')
+            print(f'{test_code:58} OK')
             self.succ_count += 1
     #:
     def setup_and_assert(self, setup_code, test_code: str):
@@ -70,9 +70,11 @@ tester = TestSuit()
 
 if TEST_ALL:
     test1 = b'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'
-    test1_rle_b = b'WW\x0cBWW\x0cBB\x03WW\x18BWW\x0e'
     test2 = b'W' * 300 + b'A' * 255 + b'B' * 19
-    test2_rle_b = b'WW\xffWW-AA\xffBB\x13'
+    test1_rle_A = b'\x0cW\x01B\x0cW\x03B\x18W\x01B\x0eW'
+    test2_rle_A = b'\xffW-W\xffA\x13B'
+    test1_rle_B = b'WW\x0cBWW\x0cBB\x03WW\x18BWW\x0e'
+    test2_rle_B = b'WW\xffWW-AA\xffBB\x13'
 
     def encode_in_mem(encode, test_data: bytes):
         in_ = io.BytesIO(test_data)
@@ -85,17 +87,21 @@ if TEST_ALL:
         return encode_in_mem(decode, rle_data)
     #:
 
+    print('\n____________ ENCODING METHOD_A\n')
+    tester.assert_("encode_in_mem(pycoder._encode_mA, test1) == test1_rle_A")
+    tester.assert_("encode_in_mem(pycoder._encode_mA, test2) == test2_rle_A")
+
+    print('\n____________ DECODING METHOD_A\n')
+    tester.assert_("decode_in_mem(pycoder._decode_mA, test1_rle_A) == test1")
+    tester.assert_("decode_in_mem(pycoder._decode_mA, test2_rle_A) == test2")
+
     print('\n____________ ENCODING METHOD_B\n')
-    rle_data1 = encode_in_mem(pycoder._encode_mB, test1)
-    tester.assert_("rle_data1 == test1_rle_b")
-    rle_data2 = encode_in_mem(pycoder._encode_mB, test2)
-    tester.assert_("rle_data2 == test2_rle_b")
+    tester.assert_("encode_in_mem(pycoder._encode_mB, test1) == test1_rle_B")
+    tester.assert_("encode_in_mem(pycoder._encode_mB, test2) == test2_rle_B")
 
     print('\n____________ DECODING METHOD_B\n')
-    plain_data = decode_in_mem(pycoder._decode_mB, test1_rle_b)
-    tester.assert_("plain_data == test1")
-    plain_data = decode_in_mem(pycoder._decode_mB, test2_rle_b)
-    tester.assert_("plain_data == test2")
+    tester.assert_("decode_in_mem(pycoder._decode_mB, test1_rle_B) == test1")
+    tester.assert_("decode_in_mem(pycoder._decode_mB, test2_rle_B) == test2")
 
     print()
     tester.summary()
